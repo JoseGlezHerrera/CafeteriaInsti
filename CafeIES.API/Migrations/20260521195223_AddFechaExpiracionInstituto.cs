c38b887436c6a2928db52c163262f8756fb3c3b1
JoseGlezHerrera <jose.gonzalezh@protonmail.com>
2026-05-22 08:42:07 +0000

    Add migration: AddFechaExpiracionInstituto

---
+++ CafeIES.API/Migrations/20260521195223_AddFechaExpiracionInstituto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeIES.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFechaExpiracionInstituto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaExpiracion",
+                table: "Institutos",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Institutos",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FechaExpiracion",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Institutos",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "FechaExpiracion",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Institutos",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "FechaExpiracion",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaExpiracion",
+                table: "Institutos");
+        }
+    }
+}
